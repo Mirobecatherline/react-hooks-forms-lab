e9ed79dafcbbd9f1e23c5a6f0b7beb20650c0db1
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-19 17:53:07 +0300

    commit tree 2c74f0dc35d1db3558f92e6d1ec6a6cefdfc2d74

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [category, setCategory]=useState('Produce');
+  const [name,setName]=useState("");
+
+  function categoryevent(event) {
+    setCategory(event.target.value);
+  }
+  function submitnamehandle(event) {
+    setName(event.target.value);
+  }
+  function submitevent(event) {
+    event.preventDefault();
+    onItemFormSubmit(
+      {
+        id: uuid(), // the `uuid` library can be used to generate a unique id
+        name,
+        category,
+      });
+    
+  }
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={submitevent}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={submitnamehandle}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={categoryevent}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
